09110f9a52d6d01d55198deeee390deb002f8402
Xavier Praveen <xavierpraveen@Xavis-Work-MacBook-Pro.local>
2022-06-13 12:32:02 +0530

    removed captcha

--- client/src/components/AddNetwork.tsx
+++ client/src/components/AddNetwork.tsx
@@ -1,49 +1,78 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 declare global {
-    interface Window {
-        ethereum: any;
-    }
+  interface Window {
+    ethereum: any;
+  }
 }
 
 const addNetwork = async (config: any) => {
-    if(!config) {
-        return;
-    }
-    if(window.ethereum == undefined) {
-        window.open('https://metamask.io/download', '_blank');
-    }
+  if (!config) {
+    return;
+  }
 
-    await window?.ethereum?.request({
-        method: 'wallet_addEthereumChain',
-        params: [{
-            chainId: `0x${config.CHAINID?.toString(16)}`,
-            chainName: config.NAME,
-            nativeCurrency: {
-                name: config.NAME,
-                symbol: config.TOKEN,
-                decimals: 18
-            },
-            rpcUrls: [config.RPC],
-            blockExplorerUrls: config.EXPLORER ? [config.EXPLORER] : null
-        }]
-    }).catch((error: any) => {
-        console.log(error)
-    });      
-}
+  if (window.ethereum == undefined) {
+    window.open("https://metamask.io/download", "_blank");
+  }
+
+  await window?.ethereum
+    ?.request({
+      method: "wallet_addEthereumChain",
+      params: [
+        {
+          chainId: `0x${config.CHAINID?.toString(16)}`,
+          chainName: config.NAME,
+          nativeCurrency: {
+            name: config.NAME,
+            symbol: config.TOKEN,
+            decimals: 18,
+          },
+          rpcUrls: [config.RPC],
+          blockExplorerUrls: config.EXPLORER ? [config.EXPLORER] : null,
+        },
+      ],
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });
+};
 
 export default function AddNetwork(props: any) {
-    return (
-        <div className='footer-buttons'>
-            <button className="add-network" onClick={() => {addNetwork(props.config)}}>
-                <img style={{width: "25px", height: "25px", marginRight: "5px"}} src="/memtamask.png"/>
-                Add Subnet to Metamask
-            </button>
+  return (
+    <>
+      <div className="footer-buttons">
+        <button
+          className="add-network"
+          onClick={() => {
+            addNetwork(props.config);
+          }}
+        >
+          <img
+            alt="metamask"
+            style={{ width: "25px", height: "25px", marginRight: "5px" }}
+            src="/memtamask.png"
+          />
+          Add Subnet to Metamask
+        </button>
 
-            <button className="add-network" onClick={() => {window.open(`${props.config.EXPLORER}`, '_blank')}}>
-                <img style={{width: "25px", height: "25px"}} src="/avaxblack.png"/>
-                View Block Explorer
-            </button>
-        </div>
-    )
+        <button
+          className="add-network"
+          onClick={() => {
+            window.open(`${props.config.EXPLORER}`, "_blank");
+          }}
+        >
+          <img
+            alt="block-explorer"
+            style={{ width: "25px", height: "25px" }}
+            src="/avaxblack.png"
+          />
+          View Block Explorer
+        </button>
+      </div>
+
+      <div className="beta-alert">
+        <p>Cannot add non-https source to metamask. Kindly add it manually.</p>
+      </div>
+    </>
+  );
 }
